8d0e010476301339fe3a365c6e9315ab1e6a1607
mirzayogy <mirza.yogy@gmail.com>
2022-05-14 19:47:05 +0800

    Menambahkan Create Tabel Pengguna

---
+++ src/sql/pengguna.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `db_212praktek6a`.`pengguna` ( `id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , `namaLengkap` VARCHAR(255) NOT NULL , `level` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
