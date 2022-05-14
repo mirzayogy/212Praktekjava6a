70b1fde2f7d6353227f8ed7b8b5482b38c8c56a2
mirzayogy <mirza.yogy@gmail.com>
2022-05-14 19:40:18 +0800

    Membuat Constructor Pengguna

--- src/model/Pengguna.java
+++ src/model/Pengguna.java
@@ -8,5 +8,14 @@ public class Pengguna {
     private String namaLengkap;
     private String level;
 
+    public Pengguna() {
+    }
 
+    public Pengguna(int id, String username, String password, String namaLengkap, String level) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.namaLengkap = namaLengkap;
+        this.level = level;
+    }
 }
