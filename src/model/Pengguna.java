211a823d2f3214f1e80240bff77aa99145e6dafa
mirzayogy <mirza.yogy@gmail.com>
2022-05-14 19:36:55 +0800

    Membuat Atribut Pengguna

--- src/model/Pengguna.java
+++ src/model/Pengguna.java
@@ -2,4 +2,11 @@ package model;
 
 public class Pengguna {
 
+    private int id;
+    private String username;
+    private String password;
+    private String namaLengkap;
+    private String level;
+
+
 }
